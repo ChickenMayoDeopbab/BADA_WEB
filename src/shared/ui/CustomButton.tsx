a24ac2832ed42cc8fb63e0caafceb128669bd7c9
전수안 <dgsw.ahn@dgsw.hs.kr>
2026-05-28 20:11:54 +0900

    feat : Custom Button

---
+++ src/shared/ui/CustomButton.tsx
@@ -0,0 +1,21 @@
+import type { ReactNode } from "react"
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  bgColor: string,
+  icon?: ReactNode,
+  label: string,
+  color?: string
+}
+
+export default function CustomButton({bgColor, icon, label, color, ...props}: ButtonProps) {
+  return (
+    <button 
+      className={`w-full h-[45px] rounded-lg flex items-center gap-3 justify-center`}
+      style={{ backgroundColor: bgColor, color }} 
+      {...props}
+    >
+      {icon}
+      <span className="font-bold text-lg">{label}</span>
+    </button>
+  )
+}
